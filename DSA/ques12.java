b7dee06959f4499a6ba78329fa9a51a9c4f51994
Biki Mukherjee <139540910+BikiMK@users.noreply.github.com>
2026-06-02 13:17:21 +0530

    left rotate the array by one

--- DSA/ques12.java
+++ DSA/ques12.java
@@ -1,9 +1,35 @@
-package basics2.DSA;
 import java.util.Scanner;
-
 
 public class ques12 {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the length of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int temp = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            arr[i - 1] = arr[i];
+        }
+
+        arr[n - 1] = temp;
+
+        System.out.println("The resultant array is: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
     }
 }
